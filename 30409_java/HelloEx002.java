53f6f61e7a7da5935c253169afd452e11893f222
setda 1494 <setda1494@gmail.com>
2022-04-12 00:06:01 +0000

    테스트용파일 업로드

---
+++ 30409_java/HelloEx002.java
@@ -0,0 +1,7 @@
+public class HelloEx002{
+    public static void main(String[] args){
+        System.out.print(100);//줄바꿈 안됨
+        System.out.println(100);//줄바꿈 됨
+        System.out.println("합격점수 : " + 100);// + 를 이용하여 문자열과 int 형을 같이 쓸수 잇음
+    }
+}
---
+++ 30409_java/HelloEx003.java
@@ -0,0 +1,10 @@
+public class HelloEx003{
+    public static void main(String[] args){
+        int x = 10;
+        int y = 20;
+        System.out.println("x + y = " + (x+y));// 더하기
+        System.out.println("x - y = " + (x-y));// 빼기
+        System.out.println("x * y = " + (x*y));// 곱하기
+        System.out.println("x / y = " + (x/y));// 나누기
+    }
+}
